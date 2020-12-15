de7500ac419e78effb4dd7690b5a9a75b08c71ee
akshaypandey44 <kumarakshaypandey@gmail.com>
2020-12-15 21:50:42 +0530

    final

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
+const posts = require('./initialData');
+
 const port = 3000
 app.use(express.urlencoded());
 
@@ -12,6 +14,38 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 // your code goes here
+let numOfApiCalls = 0;
+let initialMax = 0;
+
+app.get('/api/posts',(req,res)=>{
+
+    if(numOfApiCalls >= 5){
+        res.status(429).send({message: "Exceed Number of API Calls"})
+        return;
+    }
+
+    const parsedMax = Number(req.query.max || 10);
+    const max = parsedMax > 20 ? 10 : parsedMax;
+
+    let finalMax = max;
+    if(initialMax !== null){
+        finalMax = Math.min(finalMax,initialMax);
+    }
+
+    const topMax = posts.filter((value,idx) => idx < finalMax);
+    res.send(topMax);
+
+    if(initialMax == null){
+        initialMax = max;
+        numOfApiCalls++;
+        setTimeout(()=>{
+            initialMax = null;
+            numOfApiCalls = 0;
+        },30*1000);
+    }else{
+        numOfApiCalls++;
+    }
+})
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
