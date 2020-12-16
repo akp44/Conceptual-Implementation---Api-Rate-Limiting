64f584794f3a68913cdb1be56522e2b055d772ae
akshaypandey44 <kumarakshaypandey@gmail.com>
2020-12-16 12:29:29 +0530

    check

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ app.get('/api/posts',(req,res)=>{
     const parsedMax = Number(req.query.max || 10);
     const max = parsedMax > 20 ? 10 : parsedMax;
     let finalMax = max;
-    
+
     if(initialMax !== null){
         finalMax = Math.min(finalMax,initialMax);
     }
